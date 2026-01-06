40d8de1ed3c9f0841a84f54710e67aabf8c6e731
hyewon <84283453+kimhyew1@users.noreply.github.com>
2026-01-06 20:43:08 +0900

    Create 1_hyewon.py

---
+++ Hash/1_hyewon.py
@@ -0,0 +1,31 @@
+def solution(participant, completion) :
+    check = {}   # Key: participant's name, Value: num. of participants
+    for subject in participant:   # Count participant's frequency  (O(N))
+        if subject in check:    # Already in dictionay (= Count namesake)
+            tmp = check[subject]
+            check[subject] = tmp + 1
+        else:
+            check[subject] = 1
+
+    for subject in completion:   # Delete completion's frequency  (O(N))
+        tmp = check[subject]
+        check[subject] = tmp - 1
+
+    tmp_idx = list(check.values()).index(1)   # Always "n(participant) - n(completion) = 1" holds  (O(N))
+    answer = list(check.keys())[tmp_idx]
+    return(answer)
+
+participant = ["leo", "kiki", "eden", "eden"]
+completion = ["eden", "kiki", "leo"]
+answer = solution(participant, completion)
+answer   # "eden"
+
+# --------- Efficiency failed ---------
+# def solution(participant, completion):   O(N^2)
+#     answer = participant.copy()
+
+#     for subject in completion:   O(N)
+#         if answer.count(subject) != 0:   O(N)
+#             idx = [answer.index(subject)][0]  
+#             answer.pop(idx)  
+#     return(','.join(answer))
